2bcbfb8bfccf3617893104c2fb76b6b7a562af57
RED <purred@udid.co.kr>
2018-01-29 20:00:23 +0900

    바벨 적용

--- event.js
+++ event.js
@@ -1,3 +1,5 @@
+var EventEmitter = require('events');
+
 // 이벤트 리스너 연결 개수를 조절
 process.setMaxListeners (15);
 
@@ -13,6 +15,19 @@ process.on ("exit", function (code) {
 process.on ("exit", (code) => {
     console.log (code + " - EXIT 종료합니다.");
 });
+
+// EventEmitter 객체 생성
+let custom = new EventEmitter();
+
+custom.on("tick", (code) => {
+    console.log ("커스텀 이벤트를 실행한다.");
+});
+
+custom.emit ("tick");
+
+
+// 이벤트 강제 종료 이벤트 발생
+process.emit("exit", 2);
 
 process.on ("uncaughtException", onUncaughtException);
 
