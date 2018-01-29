f9ea11d9dab68bdc5ba56be367a2b6faccce94e0
RED <purred@udid.co.kr>
2018-01-29 16:56:41 +0900

    Node Study 시작

---
+++ hello.js
@@ -0,0 +1,14 @@
+
+console.time("Hello");
+
+console.log("dirName : " + __dirname);
+console.log("fileName : " + __filename);
+console.log("output : %d", 273);
+
+console.timeEnd("Hello");
+
+console.log (process.arch);
+console.log (process.version);
+console.log (process.platform);
+console.log (process.memoryUsage());
+
